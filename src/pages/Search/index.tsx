29e66ba34c604d4ed6d76004c595ce74011222fc
Jester <jesterjayoma04@gmail.com>
2025-02-15 22:15:27 +0800

    remove loading for search

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -11,7 +11,7 @@ import { TbMovie } from "react-icons/tb";
 import { FaArrowAltCircleUp } from "react-icons/fa";
 
 const Search = ({ query }: any) => {
-  const { searchList, isLoading, loading } = useSelector(
+  const { searchList, isLoading } = useSelector(
     (state: RootState) => state.movieList
   );
   const [page, setPage] = useState(1);
@@ -23,8 +23,6 @@ const Search = ({ query }: any) => {
   const handleScrollTop = () => {
     window.scrollTo(0, 0);
   };
-
-  console.log("load", loading);
 
   const fetchMoreShow = () => {
     const nextPage = page + 1;
@@ -40,62 +38,54 @@ const Search = ({ query }: any) => {
 
   return (
     <div>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <>
-          <InfiniteScroll
-            dataLength={searchList.length}
-            next={fetchMoreShow}
-            hasMore={searchList.length > 0}
-            loader={
-              <p className="text-center py-4 text-lightColor">
-                Loading more...
-              </p>
-            }
-            endMessage={
-              <p className="text-center py-4 text-lightColor">
-                No more TV shows to display
-              </p>
-            }
-          >
-            <div
-              className="px-3 py-5 sm:px-12 sm:pb-12 grid grid-cols-[repeat(auto-fit,minmax(130px,1fr))] 
+      <InfiniteScroll
+        dataLength={searchList.length}
+        next={fetchMoreShow}
+        hasMore={searchList.length > 0}
+        loader={
+          <p className="text-center py-4 text-lightColor">Loading more...</p>
+        }
+        endMessage={
+          <p className="text-center py-4 text-lightColor">
+            No more TV shows to display
+          </p>
+        }
+      >
+        <div
+          className="px-3 py-5 sm:px-12 sm:pb-12 grid grid-cols-[repeat(auto-fit,minmax(130px,1fr))] 
              sm:grid-cols-2 lg:grid-cols-6 gap-4"
-            >
-              {searchList?.map((show: any, index: number) => (
-                <div key={index} className="rounded-md">
-                  {show.poster_path ? (
-                    <img
-                      src={`${BASE_IMAGE_URL}${posterSize}${show.poster_path}`}
-                      alt={show.title || "Poster Image"}
-                      className="w-full h-full object-cover rounded-md"
-                      loading="lazy"
-                    />
-                  ) : (
-                    <div
-                      className="w-full h-full object-cover rounded-md bg-white 
+        >
+          {searchList?.map((show: any, index: number) => (
+            <div key={index} className="rounded-md">
+              {show.poster_path ? (
+                <img
+                  src={`${BASE_IMAGE_URL}${posterSize}${show.poster_path}`}
+                  alt={show.title || "Poster Image"}
+                  className="w-full h-full object-cover rounded-md"
+                  loading="lazy"
+                />
+              ) : (
+                <div
+                  className="w-full h-full object-cover rounded-md bg-white 
                          flex flex-col justify-center items-center gap-5 px-2"
-                    >
-                      <TbMovie className="text-primaryColor text-8xl" />
-                      <span className="sm:text-xl text-primaryColor text-center">
-                        {show.title}
-                      </span>
-                    </div>
-                  )}
+                >
+                  <TbMovie className="text-primaryColor text-8xl" />
+                  <span className="sm:text-xl text-primaryColor text-center">
+                    {show.title}
+                  </span>
                 </div>
-              ))}
+              )}
             </div>
-          </InfiniteScroll>
-          <button
-            className="text-5xl fixed bottom-10 right-5 text-red-300
+          ))}
+        </div>
+      </InfiniteScroll>
+      <button
+        className="text-5xl fixed bottom-10 right-5 text-red-300
               transition ease-in-out duration-200 hover:scale-90"
-            onClick={handleScrollTop}
-          >
-            <FaArrowAltCircleUp />
-          </button>
-        </>
-      )}
+        onClick={handleScrollTop}
+      >
+        <FaArrowAltCircleUp />
+      </button>
     </div>
   );
 };
